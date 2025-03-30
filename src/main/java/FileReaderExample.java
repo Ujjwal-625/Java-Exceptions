8e0f79d6c57ab8f08d7b8c3453a552bbd346f1f1
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-30 11:53:06 +0530

    [Author:Ujjwal] ADD:FileReaderExample.java

---
+++ src/main/java/FileReaderExample.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class FileReaderExample {
+    public static void main(String[] args) {
+        String fileName = "data.txt";  // File to read
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            System.out.println("File Contents:");
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+        }
+    }
+}
